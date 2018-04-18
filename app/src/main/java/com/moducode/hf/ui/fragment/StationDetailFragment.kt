e9a21fbba807c84fd32677cbd876459cedb710f5
Justin Inácio <j.trollkarl@gmail.com>
2018-04-18 19:54:26 +0200

    update
    - added more details to StationDetailFragment

--- app/src/main/java/com/moducode/hf/ui/fragment/StationDetailFragment.kt
+++ app/src/main/java/com/moducode/hf/ui/fragment/StationDetailFragment.kt
@@ -6,9 +6,12 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 
 import com.moducode.hf.R
 import com.moducode.hf.data.Program
+import com.moducode.hf.toDp
 import kotlinx.android.synthetic.main.fragment_station_detail.*
 
 
@@ -38,6 +41,10 @@ class StationDetailFragment : Fragment() {
 
         val program: Program? = arguments?.getParcelable(PROGRAM_KEY)
 
-        tv_station_name.text = program?.description
+        tv_station_name.text = context?.getString(R.string.station_name, program?.name)
+        tv_station_email.text = context?.getString(R.string.station_email, program?.email)
+        tv_station_channel.text = context?.getString(R.string.station_channel, program?.channel?.id.toString())
+        
+        Glide.with(view.context).applyDefaultRequestOptions(RequestOptions().fitCenter()).load(program?.programimagetemplate).into(iv_program_image)
     }
 }
